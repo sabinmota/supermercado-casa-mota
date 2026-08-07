63bd6a24e967d571d7e26cc9f2d8a65ecff2bc7f
sabinmota <samora027@gmail.com>
2026-08-07 19:44:27 -0400

    Borrado de varios archivos

--- 7-borrar-respaldo.sql
+++
@@ -1,6 +0,0 @@
-DROP SCHEMA IF EXISTS respaldo CASCADE;
-
-SELECT 'respaldo eliminado - ya no se puede deshacer' AS resultado,
-       count(*)::text                                 AS esquemas_respaldo_restantes
-FROM information_schema.schemata
-WHERE schema_name = 'respaldo';
